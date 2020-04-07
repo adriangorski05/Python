69a930302cf5ce7da32dce55835b232818c4c820
adriangorski05 <60521764+adriangorski05@users.noreply.github.com>
2020-04-07 17:28:07 +0200

    Add files via upload

--- trojkat.py
+++ trojkat.py
@@ -7,17 +7,15 @@ x=max(dlugosci_bokow)
 
 dlugosci_bokow.remove(x)
 
-for i in dlugosci_bokow:
-    if sum(dlugosci_bokow) > x:
-        answer=input("Z takich odcinkow da sie zbudowac trojkat, czy chcesz policzyc jego powierzchnie? (y/n) ")
-        if answer == "y":
-            ob=0.5*(a+b+c)
-            pole=(ob*(ob-a)*(ob-b)*(ob-c))**0.5
-            print("Pole trojkata wynosi: ", pole)
-            break
-        if answer == "n":
-            print("Koniec")
-            break
-    else:
-        print("Z takich odcinkow nie da sie zbudowac trojkata.")
-        break
+
+if sum(dlugosci_bokow) > x:
+    answer=input("Z takich odcinkow da sie zbudowac trojkat, czy chcesz policzyc jego powierzchnie? (y/n) ")
+    if answer == "y":
+        ob=0.5*(a+b+c)
+        pole=(ob*(ob-a)*(ob-b)*(ob-c))**0.5
+        print("Pole trojkata wynosi: ", pole)
+    if answer == "n":
+        print("Koniec")
+        
+else:
+    print("Z takich odcinkow nie da sie zbudowac trojkata.")
